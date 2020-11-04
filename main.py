22a0d0b3713b6f81ba721946273ff4ba16dfe110
dancorrea86 <danielmacielcorrea@gmail.com>
2020-11-03 21:49:49 -0300

    first

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+scores_df = pd.read_excel('sample.xlsx')
+#print(scores_df)
+
+scores_df['average'] = scores_df.mean(axis=1)
+
+scores_df['Pass/Fail'] = np.where(scores_df['average'] > 60, 'Pass', 'Fail')
+
+conditions = [
+    (scores_df['average'] >= 90),
+    (scores_df['average'] < 90) & (scores_df['average']>= 80),
+    (scores_df['average'] < 90) & (scores_df['average']>= 70),
+    (scores_df['average'] < 70) & (scores_df['average']>= 60),
+    (scores_df['average'] < 60)
+]
+results = ['A', 'B', 'C', 'D', 'F']
+
+scores_df['Letter Grade'] = np.select(conditions, results)
+#print(scores_df)
+
+scores_df.to_excel("novaSheet.xlsx")
